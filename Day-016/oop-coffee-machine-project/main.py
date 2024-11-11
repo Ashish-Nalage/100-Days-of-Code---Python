98ec592be1fdc4839f4ce3fce32fc98c2e9e0b51
Ashish-Nalage <nalageashish2005@gmail.com>
2024-11-11 20:39:44 +0530

    Add Day-016 project code

---
+++ Day-016/oop-coffee-machine-project/main.py
@@ -0,0 +1,9 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+# obj = Menu()
+# print(obj.get_items())
+
+obj2 = MenuItem('water', 'milk', 'coffee')
+print(obj2.ingredients)

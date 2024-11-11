8157398ef4a8c4a195d244c394928f2f64ff1c30
Ashish-Nalage <nalageashish2005@gmail.com>
2024-11-11 20:43:30 +0530

    Update Day-016

--- Day-016/oop-coffee-machine-project/main.py
+++ Day-016/oop-coffee-machine-project/main.py
@@ -1,9 +1,3 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
-
-# obj = Menu()
-# print(obj.get_items())
-
-obj2 = MenuItem('water', 'milk', 'coffee')
-print(obj2.ingredients)

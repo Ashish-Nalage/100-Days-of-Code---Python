ed0fe0ea2f28a26aa7714bdc75464b9b4989c86b
Ashish-Nalage <nalageashish2005@gmail.com>
2024-11-27 09:59:54 +0530

    Update pong_game code

---
+++ Day-022/pong_game/ball.py
@@ -0,0 +1,13 @@
+from turtle import Turtle
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.penup()
+        self.shape("circle")
+        self.color("white")
+        self.setheading(45)
+
+
+    def move(self):
+        self.forward(10)
--- Day-022/pong_game/main.py
+++ Day-022/pong_game/main.py
@@ -1,7 +1,32 @@
 from turtle import Screen 
+from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
-screen.setup(width=1600, height=1200)
+screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("pong_game")
+screen.tracer(0)
+
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up,"Up")
+screen.onkey(r_paddle.go_down,"Down")
+screen.onkey(l_paddle.go_up,"w")
+screen.onkey(l_paddle.go_down,"s")
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+
+    
+
 screen.exitonclick()

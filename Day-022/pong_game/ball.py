3e7288efcebfced62de7f80f1cd2ac272015a923
Ashish-Nalage <nalageashish2005@gmail.com>
2024-11-27 14:02:01 +0530

    Update pong_game code

--- Day-022/pong_game/ball.py
+++ Day-022/pong_game/ball.py
@@ -6,8 +6,10 @@ class Ball(Turtle):
         self.penup()
         self.shape("circle")
         self.color("white")
-        self.setheading(45)
 
 
     def move(self):
-        self.forward(10)
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+
+        self.goto(new_x, new_y)

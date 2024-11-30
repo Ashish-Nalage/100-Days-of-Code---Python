06237a336af16a0823fcaa84ad5dfc01dd1f4ea9
Ashish-Nalage <nalageashish2005@gmail.com>
2024-11-30 16:07:10 +0530

    Add High Score in snake_game and Add data.txt for storing the high score

--- Day-020_and_021/snake_game/main.py
+++ Day-020_and_021/snake_game/main.py
@@ -6,11 +6,11 @@ from scoreboard import Scoreboard
 
 screen = Screen()
 
-screen.setup(width=600,height=600)
+screen.setup(width=600,height=600)  # setting up screen
 screen.title("This is an snake game")
 screen.bgcolor("black")
 
-screen.tracer(0)
+screen.tracer(0) # every thing after this is on canvs but aint visible
 
 
 snake = Snake()
@@ -26,24 +26,27 @@ screen.onkey(snake.down, "Down")
 game_is_on = True 
 
 while game_is_on:
-    screen.update()
+    screen.update() # canvas updated with 0.1 sec delay
     time.sleep(0.1)
 
     snake.move()
     
-    if snake.head.distance(food) < 15:
+    if snake.head.distance(food) < 15: # detect collition with food
+        scoreboard.score += 1
         food.refresh()
         snake.extend()
         scoreboard.update_score()
 
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.xcor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280: # detect collition with walls
+        scoreboard.reset()
+        scoreboard.update_score()
+        snake.reset()
 
-    for segment in snake.segments[1:]:
+    for segment in snake.segments[1:]: # detect collition with snake body
         
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            scoreboard.update_score()
+            snake.reset()
 
 screen.exitonclick()
--- Day-020_and_021/snake_game/scoreboard.py
+++ Day-020_and_021/snake_game/scoreboard.py
@@ -6,7 +6,9 @@ FONT = ("courier", 20, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
-        self.score = -1
+        self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.hideturtle()
         self.goto(0, 260)
@@ -14,10 +16,12 @@ class Scoreboard(Turtle):
         self.update_score()
 
     def update_score(self):
-        self.score += 1
         self.clear()
-        self.write(f"Score: {self.score}", align=CENTER, font=FONT )
+        self.write(f"Score: {self.score} HighScore: {self.high_score}", align=CENTER, font=FONT )
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over", align=CENTER, font=FONT )
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.high_score))
+        self.score = 0

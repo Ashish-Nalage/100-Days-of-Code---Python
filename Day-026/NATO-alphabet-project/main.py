7a0e45dd1dc46b4ad1f652eb7f193529fb9c8482
Ashish-Nalage <nalageashish2005@gmail.com>
2024-12-13 16:24:10 +0530

    Add exception handling in the NATO phonetic alphabet project

--- Day-026/NATO-alphabet-project/main.py
+++ Day-026/NATO-alphabet-project/main.py
@@ -1,10 +1,6 @@
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-# #TODO 1. Create a dictionary in this format:
-# {"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
 import pandas
 
@@ -14,10 +10,17 @@ df = pandas.read_csv("./Day-026/NATO-alphabet-project/nato_phonetic_alphabet.csv
 # make dictionary form dataframe
 nato_dict = {row.letter:row.code for (index,row) in df.iterrows()}
 
-# ask user for input
-word = input("what is your word?: ").upper()
 
-output_list = [nato_dict[letter] for letter in word]
-print(output_list)
+def generate_phonetics():
+    word = input("what is your word?: ").upper()    # ask user for input
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetics()
+    else:
+        print(output_list)
+
+generate_phonetics()
 
 
